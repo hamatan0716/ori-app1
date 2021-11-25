3ab3321382dbc690ece5989f13c9b03f8fa4c56a
hamatan0716 <hama.uvuv0716@gmail.com>
2021-11-25 15:32:16 +0900

    createアクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(prams_item)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def prams_item
+    params.require(:item).permit(:name, :introduction, :genre_id, :image).merge(user_id: current_user.id)
   end
 end
