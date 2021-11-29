a305dfaba98292510742c7e6c86ad6992a57b35d
hamatan0716 <hama.uvuv0716@gmail.com>
2021-11-29 17:08:06 +0900

    フロント作成

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveHash::Base
   self.data = [
-    { id: 1, name: 'ジャンルを選択してください' },
+    { id: 1, name: '--ジャンルを選択してください--' },
     { id: 2, name: 'PC周辺機器' },
     { id: 3, name: 'オーディオ' },
     { id: 4, name: '家電' },
@@ -10,7 +10,8 @@ class Genre < ActiveHash::Base
     { id: 8, name: '収納' },
     { id: 9, name: 'インテリア' },
     { id: 10, name: '時計' },
-    { id: 11, name: 'その他' }
+    { id: 11, name: 'キャンプ用品' },
+    { id: 12, name: 'その他'}
   ]
   
   include ActiveHash::Associations
