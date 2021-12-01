c4ff3ef87c059d4f00ac2a00b59a9d8e00489f3c
hamatan0716 <hama.uvuv0716@gmail.com>
2021-12-01 18:07:00 +0900

    マイグレーション

--- db/migrate/20211201085707_create_wants.rb
+++ db/migrate/20211201085707_create_wants.rb
@@ -1,7 +1,9 @@
 class CreateWants < ActiveRecord::Migration[6.0]
   def change
     create_table :wants do |t|
-
+      t.references :user,        null: false, foreign_key: true
+      t.references :item,        null: false, foreign_key: true
+      
       t.timestamps
     end
   end
