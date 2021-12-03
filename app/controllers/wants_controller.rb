3e516135339b010ee69d2225a656399fbb7b477b
hamatan0716 <hama.uvuv0716@gmail.com>
2021-12-03 21:39:42 +0900

    非同期いいね

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -1,10 +1,15 @@
 class WantsController < ApplicationController
+  before_action :item_params
   def create
     Want.create(user_id: current_user.id, item_id: params[:id])
-    redirect_to item_path(params[:id])
   end
   def destroy
     Want.find_by(user_id: current_user.id, item_id: params[:id]).destroy
-    redirect_to item_path(params[:id])
+  end
+
+  private
+
+  def item_params
+    @item = Item.find(params[:id])
   end
 end
